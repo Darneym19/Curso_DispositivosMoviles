fa31c7189473157763acddadd32fbcb25d56477c
Darney Maya <100388710+Darneym19@users.noreply.github.com>
2023-02-20 18:43:51 -0500

    Clase13Feb

---
+++ Clase13Feb/app/src/main/java/com/mobie/applicationdos/MainActivity.java
@@ -0,0 +1,18 @@
+package com.aplicaciondos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "ActividadDos";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"--OnCreate--");
+    }
+}
